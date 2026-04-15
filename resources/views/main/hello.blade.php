42bf2898e9fabd60f5588ad81005da98a137b9fe
Artem Moroz <juniorbilli1488@gmail.com>
2026-04-15 16:16:47 +0300

    Задание 1: новостной сайт на Laravel

---
+++ resources/views/main/hello.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+@section('content')
+    <div class="jumbotron mt-4">
+        <h1 class="display-4">Добро пожаловать на новостной портал!</h1>
+        <p class="lead">Здесь вы найдете самые актуальные новости и события.</p>
+        <hr class="my-4">
+        <p>Скоро здесь появится список новостей...</p>
+    </div>
+@endsection
